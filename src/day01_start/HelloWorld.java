69d6a5ca921093840764560aa2d3ccc5e16521dc
DeskTop <16713780871@163.com>
2026-03-25 08:28:03 +0800

    feat:2026年3月25日学习了Java的变量

--- src/day01_start/HelloWorld.java
+++ src/day01_start/HelloWorld.java
@@ -1,3 +1,5 @@
+package day01_start;
+
 public class HelloWorld {
     public static void main(String[] args){
         //2026年3月24日：今天是学java的第一天，虽然之前也学过，但是跟之前相比用了git
---
+++ src/day02_variable/Variable.java
@@ -0,0 +1,10 @@
+package day02_variable;
+
+public class Variable {
+    public static void main(String[] args) {
+        //2026年3月25日 今天学习了变量，等自考本考完要全部投入到Java中
+        System.out.println("变量：");
+        int age = 18;
+        System.out.println(age);
+    }
+}
